3490622a9cf651ff59270bd3e07d041874c7f18b
valdeezzee <jvalderrama91@knights.ucf.edu>
2017-12-09 16:31:51 -0500

    New prime numbers script

---
+++ PrimeNumbersScript.py
@@ -0,0 +1,12 @@
+primeList = []
+# check for factors
+for num in range(2,20):
+    if num > 1 and len(primeList) != 5:
+        for i in range(2,num):
+            if (num % i) == 0:
+               break
+        else:
+            primeList.append(num)
+            print(num,"is a prime number")
+       
+print(primeList)
