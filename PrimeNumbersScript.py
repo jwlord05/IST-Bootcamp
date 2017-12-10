36766e9576e9afd8191909195f1f6b41dba6c108
Unknown <jwlord05@knights.ucf.edu>
2017-12-10 12:53:38 -0500

    Small change to code

--- PrimeNumbersScript.py
+++ PrimeNumbersScript.py
@@ -10,3 +10,21 @@ for num in range(2,20):
             print(num,"is a prime number")
        
 print(primeList)
+
+'''
+
+primeList = [] 
+# check for factors 
+for num in range(2,101): 
+    if num > 1 and len(primeList) != 5: 
+        for i in range(2,num): 
+            if (num % i) == 0: 
+               break 
+        else: 
+            primeList.append(num) 
+            
+            
+print (primeList)
+
+
+'''
